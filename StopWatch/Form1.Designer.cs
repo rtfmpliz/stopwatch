dcea8d11512644cc2fcb24ae83d52daf36f0d302
wahyu <wahyu@istw.co.id>
2022-01-21 10:15:01 +0700

    start-stop-only

--- StopWatch/Form1.Designer.cs
+++ StopWatch/Form1.Designer.cs
@@ -31,24 +31,51 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(201, 170);
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 59);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 87);
+            this.button1.Size = new System.Drawing.Size(106, 80);
             this.button1.TabIndex = 0;
             this.button1.Text = "START";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("LCD", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(3, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(128, 147);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
 
@@ -58,5 +85,6 @@
 
         private System.Windows.Forms.Timer timer1;
         private Button button1;
+        private Label label1;
     }
 }
--- StopWatch/Form1.cs
+++ StopWatch/Form1.cs
@@ -1,10 +1,64 @@
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+
 namespace StopWatch
 {
     public partial class Form1 : Form
     {
+        System.Windows.Forms.Timer timer;
+        Stopwatch sw;
         public Form1()
         {
             InitializeComponent();
+
+                this.StartPosition = FormStartPosition.Manual;
+                this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - this.Width, Screen.PrimaryScreen.WorkingArea.Height - this.Height);
+            GraphicsPath p = new GraphicsPath();
+            p.AddEllipse(1, 1, button1.Width - 4, button1.Height - 4);
+            button1.Region = new Region(p);
+        }
+        public class RoundButton : Button
+        {
+            protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+            {
+                GraphicsPath grPath = new GraphicsPath();
+                grPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+                this.Region = new System.Drawing.Region(grPath);
+                base.OnPaint(e);
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "START")
+            {
+                timer = new System.Windows.Forms.Timer();
+                timer.Interval = (1000);
+                timer.Tick += new EventHandler(timer1_Tick);
+                sw = new Stopwatch();
+                timer1.Start();
+                sw.Start();
+                button1.Text = "STOP";
+                button1.BackColor = Color.Red;
+            }
+            else
+            {
+                timer1.Stop();
+                sw.Stop();
+                button1.Text = "START";
+                button1.BackColor = Color.Green;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = sw.Elapsed.Hours.ToString("00") + ":" + sw.Elapsed.Minutes.ToString("00") +":"+ sw.Elapsed.Seconds.ToString("00");
+            Application.DoEvents();
         }
     }
 }
