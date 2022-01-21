8fe9a53acb8b60ddd092e3ba96a2be6e05b62d62
wahyu <wahyu@istw.co.id>
2022-01-21 14:20:44 +0700

    printscreen-key

--- StopWatch/Form1.cs
+++ StopWatch/Form1.cs
@@ -25,15 +25,19 @@ namespace StopWatch
         //https://stackoverflow.com/questions/19960438/timer-updating-label/19962989
         System.Windows.Forms.Timer timer;
         Stopwatch sw;
+        private KeyHandler ghk;
         public Form1()
         {
+            ghk = new KeyHandler(Keys.PrintScreen, this);
+            ghk.Register();
+
             InitializeComponent();
             //winform on bottom-right
             // https://stackoverflow.com/questions/1385674/place-winform-on-bottom-right
-                this.StartPosition = FormStartPosition.Manual;
-                //this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - this.Width, Screen.PrimaryScreen.WorkingArea.Height - this.Height);
+            this.StartPosition = FormStartPosition.Manual;
+            //this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - this.Width, Screen.PrimaryScreen.WorkingArea.Height - this.Height);
             //middle top center    
-            this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width /2 - this.Width /2, 0);
+            this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width / 2 - this.Width / 2, 0);
             this.TopMost = true;
 
             //round-shape-button
@@ -41,7 +45,7 @@ namespace StopWatch
             GraphicsPath p = new GraphicsPath();
             p.AddEllipse(1, 1, button1.Width - 4, button1.Height - 4);
             button1.Region = new Region(p);
-            
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -49,7 +53,9 @@ namespace StopWatch
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        //encaspulate methad call in a button click
+        //https://social.msdn.microsoft.com/Forums/windows/en-US/26546603-748f-482c-8f4d-ad454663bb9e/how-to-call-button1clickobject-sender-eventargs-e-from-other-method?forum=winforms
+        void StartTimer()
         {
             if (button1.Text == "START")
             {
@@ -69,12 +75,17 @@ namespace StopWatch
                 button1.Text = "START";
                 button1.BackColor = Color.Green;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartTimer();
 
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Text =  sw.Elapsed.Minutes.ToString("00") +":"+ sw.Elapsed.Seconds.ToString("00");
+            label1.Text = sw.Elapsed.Minutes.ToString("00") + ":" + sw.Elapsed.Seconds.ToString("00");
             Application.DoEvents();
         }
 
@@ -82,6 +93,14 @@ namespace StopWatch
         {
             SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
 
+        }
+        //detect key form not focused
+        //https://stackoverflow.com/questions/18291448/how-do-i-detect-keypress-while-not-focused
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+                StartTimer();
+            base.WndProc(ref m);
         }
     }
 }
